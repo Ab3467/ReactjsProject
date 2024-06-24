db8d4af862621a12d62431419efac32089e20427
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-24 11:32:56 +0500

    comment

--- react-app/src/components/Header.js
+++ react-app/src/components/Header.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import logo from "../../src/assets/logo.jpg"
+import React from "react";
+import logo from "../../src/assets/logo.jpg";
 
 export default function Header() {
   return (
-    <header>
-        <img src={logo} alt="logo" />
-        <h1>Investment Calculator</h1>
-    </header>
-  )
+    <div className="bg-blue-100 text-center font-semibold flex flex-col items-center py-4">
+      <img
+        src={logo}
+        alt="logo"
+        className="object-contain bg-transparent h-10 w-10 mr-4"
+      />
+      <h1 className="text-lg">Investment Calculator</h1>
+    </div>
+  );
 }
