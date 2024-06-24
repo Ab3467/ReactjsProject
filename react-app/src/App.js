96ab179897e84a94b5328c428e3461f6c702b081
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-24 16:17:01 +0500

    comment

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
-
+import { useState, useEffect, useRef } from 'react';
 
 function App() {
+  const [count, setcount] = useState();
+ const btnref = useRef();
+
+  useEffect(() => {
+    btnref.current.style.backgroundColor = "red"
+  })
+  
+
   return (
   <>
- 
+   <div className=" text-center flex justify-center">
+     <button onClick={setcount(count+1)} ref={btnref}>count is {count}</button>
+   </div>
   </>
   );
 }
