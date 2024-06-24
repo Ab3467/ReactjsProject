b5b093cb7a7ae41fa0c012cb9cff453bac8c0d9f
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-24 11:16:10 +0500

    comment

---
+++ react-app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from "../../src/assets/"
+
+export default function Header() {
+  return (
+    <header>
+        <img src={} alt="" />
+        <h1>Investment Calculator</h1>
+    </header>
+  )
+}
