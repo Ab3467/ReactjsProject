cc36cc37e76d138ebe4082f8b3c4f21d6b7bab19
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-24 18:33:54 +0500

    Comment

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -15,11 +15,11 @@ function App() {
   const memoizedValue = useMemo(() => expensiveComputation(count), [count]);
 
   return (
-    <div className="App">
-      <h1>useMemo Example</h1>
+    <div className=" text-center m-5">
+      <h1 className=' text-black font-semibold'>useMemo Example</h1>
       <div>
         <p>Count: {count}</p>
-        <button onClick={() => setCount(count + 1)}>Increment Count</button>
+        <button onClick={() => setCount(count + 1)} className=' border border-black w-36 h-10 rounded-sm bg-gray-300 m-5 p-2' >Increment Count</button>
       </div>
       <div>
         <p>Other Count: {otherCount}</p>
