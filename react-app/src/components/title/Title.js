82373dc59394ebf885eb1ba186918ffb22107553
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-25 17:53:09 +0500

    Changes

--- react-app/src/components/title/Title.js
+++ react-app/src/components/title/Title.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./Title.css"
 
-export default function Title() {
+
+export default function Title(props) {
   return (
     <div className='title'>
-      <p>Our Program</p>
-      <h2>What We Offer</h2>
+      <p>{props.subTitle}</p>
+      <h2>{props.title}</h2>
     </div>
   )
 }
