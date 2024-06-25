dd4c2330a76814266976b7fb6bba400294f2183b
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-25 17:38:41 +0500

    Changes

---
+++ react-app/src/components/title/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Title.css"
+
+export default function Title() {
+  return (
+    <div className='title'>
+      <p>Our Program</p>
+      <h2>What We Offer</h2>
+    </div>
+  )
+}
