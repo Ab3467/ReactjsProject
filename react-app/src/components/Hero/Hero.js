3d7b82f1afe2283de43de956cb5bec4d412bec85
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-25 16:01:51 +0500

    Changes

--- react-app/src/components/Hero/Hero.js
+++ react-app/src/components/Hero/Hero.js
@@ -5,7 +5,11 @@ import "./Hero.css"
 export default function Hero() {
   return (
     <div className='hero'>
-      
+      <div className="hero-text">
+        <h1>We Ensure better education for a better world</h1>
+        <p>Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education</p>
+        <button className='btn'>Explore more</button>
+      </div>
     </div>
   )
 }
