a16af0bb6927189d23619ad2e77e7394bfa84af0
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-25 17:46:40 +0500

    Changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,6 +2,7 @@
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Program from "./components/program/Program";
+import Title from "./components/title/Title";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <div className="container">
+        <Title/>
         <Program />
       </div>
     </div>
