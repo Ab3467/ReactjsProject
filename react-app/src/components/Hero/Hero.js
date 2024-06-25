31f824df632f7a43832bb5de499021b9d6eb81e9
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-25 16:44:26 +0500

    Changes

--- react-app/src/components/Hero/Hero.js
+++ react-app/src/components/Hero/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "../Hero/Hero.js"
 import "./Hero.css"
+import ArrowIcon from "../../assets/dark-arrow.png"
 
 export default function Hero() {
   return (
@@ -8,7 +9,7 @@ export default function Hero() {
       <div className="hero-text">
         <h1>We Ensure better education for a better world</h1>
         <p>Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education</p>
-        <button className='btn'>Explore more</button>
+        <button className='btn'>Explore more <img src={ArrowIcon} alt="" /></button>
       </div>
     </div>
   )
