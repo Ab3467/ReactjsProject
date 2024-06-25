8ef1d0630abce03cc07397e0542c8cceb086dfc1
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-25 17:19:45 +0500

    Changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,6 +3,7 @@ import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Program from "./components/program/Program";
 
+
 function App() {
   return (
     <div>
--- react-app/src/components/program/Program.js
+++ react-app/src/components/program/Program.js
@@ -3,6 +3,9 @@ import "./Program.css"
 import Program1 from "../../assets/program-1.png"
 import Program2 from "../../assets/program-2.png"
 import Program3 from "../../assets/program-3.png"
+import Program_icon_1 from "../../assets/program-icon-1.png"
+import Program_icon_2 from "../../assets/program-icon-2.png"
+import Program_icon_3 from "../../assets/program-icon-3.png"
 
 export default function Program() {
   return (
