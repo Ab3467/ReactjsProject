1099ad1ab0fd4ee8f61d8d745dfbdf4dc5575223
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-25 14:56:43 +0500

    Changes

--- react-app/src/components/Navbar/Navbar.js
+++ react-app/src/components/Navbar/Navbar.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import "../Navbar/Navbar.css"
-import assets from "../../assets/logo.png"
+import logo from "../../assets/logo.png"
 
 export default function Navbar() {
   return (
     <nav>
-        <img src={} alt="" />
+        <img src={logo} alt="" />
+        <ul>
+            <li>Home</li>
+            <li>Program</li>
+            <li>About us</li>
+            <li>Campus</li>
+            <li>Testimonials</li>
+        </ul>
     </nav>
   )
 }
