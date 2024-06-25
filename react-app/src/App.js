da148a5e4464e3d99e95aa557af2b6e671574228
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-25 17:08:37 +0500

    Changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,14 @@
 // import React, { useState, useMemo } from 'react';
 import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
+import Program from './components/program/Program';
 
 function App() {
   return(
     <div>
       <Navbar/>
       <Hero/>
+      <Program/>
     </div>
   );
 }
