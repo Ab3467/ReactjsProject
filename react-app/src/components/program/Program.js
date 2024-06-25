9b8f6b5a4bf1b53c2b95419ab7cc3ed5114ec5f4
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-25 17:06:22 +0500

    Changes

--- react-app/src/components/program/Program.js
+++ react-app/src/components/program/Program.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import "./Program.css"
+import Program1 from "../../assets/program-1.png"
+import Program2 from "../../assets/program-2.png"
+import Program3 from "../../assets/program-3.png"
 
 export default function Program() {
   return (
-    <div>
-      
+    <div className='programs'>
+      <div className="program">
+        <img src={Program1} alt="" />
+      </div>
+      <div className="program">
+        <img src={Program2} alt="" />
+      </div>
+      <div className="program">
+        <img src={Program3} alt="" />
+      </div>
     </div>
   )
 }
