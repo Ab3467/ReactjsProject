27d1e4527c6d287a02206a78f19e2a90ece843c0
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-26 15:04:58 +0500

    Changes

--- react-app/src/components/Testimonials/Testimonial.js
+++ react-app/src/components/Testimonials/Testimonial.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Testimonial.css";
 import back_icon from "../../assets/back-icon.png";
 import next_icon from "../../assets/next-icon.png";
@@ -8,12 +8,28 @@ import user_3 from "../../assets/user-3.png";
 import user_4 from "../../assets/user-4.png";
 
 export default function Testimonial() {
+  const slider = useRef();
+  let tx= 0;
+
+  const slideForward = () => {
+    if(tx > -50){
+        tx -= 25;
+    }
+    slider.current.style.transform = `translateX(${tx}%)`
+  };
+  const slideBackward = () => {};
+
   return (
     <div className="testimonails">
-      <img src={next_icon} alt="" className="next-btn" onClick={slideForward}/>
-      <img src={back_icon} alt="" className="back-btn" onClick={slideBackward}/>
+      <img src={next_icon} alt="" className="next-btn" onClick={slideForward} />
+      <img
+        src={back_icon}
+        alt=""
+        className="back-btn"
+        onClick={slideBackward}
+      />
       <div className="slider">
-        <ul>
+        <ul ref={slider}>
           <li>
             <div className="slide">
               <div className="user-info">
