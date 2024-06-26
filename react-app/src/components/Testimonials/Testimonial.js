26f4eda30fd251b1ca767582ddd37e6c531befa4
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-26 12:52:35 +0500

    Changes

--- react-app/src/components/Testimonials/Testimonial.js
+++ react-app/src/components/Testimonials/Testimonial.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import back_icon from "../../assets/back-icon.png"
 import next_icon from "../../assets/next-icon.png"
+import user_1 from "../../assets/user-1.png"
+import user_2 from "../../assets/user-2.png"
+import user_3 from "../../assets/user-3.png"
+import user_4 from "../../assets/user-4.png"
 
 export default function Testimonial() {
   return (
