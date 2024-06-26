b9a8a68eff34f617415d978513855c97474facf7
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-26 12:53:16 +0500

    Changes

--- react-app/src/components/Testimonials/Testimonial.js
+++ react-app/src/components/Testimonials/Testimonial.js
@@ -10,7 +10,7 @@ export default function Testimonial() {
   return (
     <div className='testimonails'>
       <img src={next_icon} alt="" className='next-icon'/>
-
+      <img src={back_icon} alt="" className='back-icon'/>
     </div>
   )
 }
