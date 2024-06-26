3b28d09cff0e8d3599fb656b9bedba816ddca886
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-26 13:14:51 +0500

    Changes

--- react-app/src/components/Testimonials/Testimonial.js
+++ react-app/src/components/Testimonials/Testimonial.js
@@ -1,32 +1,38 @@
-import React from 'react'
-import "./Testimonial.css"
-import back_icon from "../../assets/back-icon.png"
-import next_icon from "../../assets/next-icon.png"
-import user_1 from "../../assets/user-1.png"
-import user_2 from "../../assets/user-2.png"
-import user_3 from "../../assets/user-3.png"
-import user_4 from "../../assets/user-4.png"
+import React from "react";
+import "./Testimonial.css";
+import back_icon from "../../assets/back-icon.png";
+import next_icon from "../../assets/next-icon.png";
+import user_1 from "../../assets/user-1.png";
+import user_2 from "../../assets/user-2.png";
+import user_3 from "../../assets/user-3.png";
+import user_4 from "../../assets/user-4.png";
 
 export default function Testimonial() {
   return (
-    <div className='testimonails'>
-      <img src={next_icon} alt="" className='next-btn'/>
-      <img src={back_icon} alt="" className='back-btn'/>
+    <div className="testimonails">
+      <img src={next_icon} alt="" className="next-btn" />
+      <img src={back_icon} alt="" className="back-btn" />
       <div className="slider">
-      <ul>
-        <li>
+        <ul>
+          <li>
             <div className="slide">
-                <div className="user-info">
-                    <img src={user_1} alt="" />
-                    <div>
-                        <h3>William Jackson</h3>
-                        <span>Edusity, USA</span>
-                    </div>
+              <div className="user-info">
+                <img src={user_1} alt="" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
                 </div>
+              </div>
+              <p>
+                Choosing to pursue my degree at Edusity was one of the best
+                decisions I've ever made. The supportive community,
+                state-of-the-art facilities, and commitment to academic
+                excellence have truly exceeded my expectations.
+              </p>
             </div>
-        </li>
-      </ul>
+          </li>
+        </ul>
       </div>
     </div>
-  )
+  );
 }
