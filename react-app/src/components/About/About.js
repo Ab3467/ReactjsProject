9060392d674471788bade90f196fd39a44467cd8
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-26 10:57:44 +0500

    Changes

--- react-app/src/components/About/About.js
+++ react-app/src/components/About/About.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./About.css"
-// import
+import about_img from "../../assets/about.png"
+import play_icon from "../../assets/play_icon.png"
 
 export default function About() {
   return (
     <div className='about'>
       <div className="about-left">
-      
+       <img src={about_img} alt="" className='aboout_img'/>
+       <img src={play_icon} alt="" className='play_icon'/>
       </div>
       <div className="about-right">
       
