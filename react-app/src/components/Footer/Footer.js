57a8a45ee382b6083c46afb0faee5c6d44f3f891
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-26 17:08:22 +0500

    Changes

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -3,8 +3,12 @@ import "./Footer.css"
 
 export default function Footer() {
   return (
-    <div>
-      
+    <div className='footer'>
+      <p>© 2024 Edusity. All rights reserved.</p>
+      <ul>
+        <li>Terms of services</li>
+        <li>Private Policy</li>
+      </ul>
     </div>
   )
 }
