fa385b9488344da627153bc1684473e0413d83c0
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-26 11:00:45 +0500

    Changes

--- react-app/src/components/About/About.js
+++ react-app/src/components/About/About.js
@@ -17,14 +17,15 @@ export default function About() {
           Embark on a transformative educational journey with our university's
           comprehensive education programs. Our cutting-edge curriculum is
           designed to empower students with the knowledge, skills, and
-          experiences needed to excel in the dynamic field of education. With a
+          experiences needed to excel in the dynamic field of education.</p>
+          <p>With a
           focus on innovation, hands-on learning, and personalized mentorship,
           our programs prepare aspiring educators to make a meaningful impact in
-          classrooms, schools, and communities. Whether you aspire to become a
+          classrooms, schools, and communities.</p>
+          <p>Whether you aspire to become a
           teacher, administrator, counselor, or educational leader, our diverse
           range of programs offers the perfect pathway to achieve your goals and
-          unlock your full potential in shaping the future of education.
-        </p>
+          unlock your full potential in shaping the future of education.</p>
       </div>
     </div>
   );
