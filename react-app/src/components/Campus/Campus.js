9454a2694f20b3d3c98f020e356864dde8e9f2a5
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-26 11:45:47 +0500

    Changes

---
+++ react-app/src/components/Campus/Campus.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import g_1 from "../../assets/gallery-1.png"
+import g_2 from "../../assets/gallery-2.png"
+import g_3 from "../../assets/gallery-3.png"
+import g_4 from "../../assets/gallery-4.png"
+
+export default function Campus() {
+  return (
+    <div className='campus'>
+      
+    </div>
+  )
+}
