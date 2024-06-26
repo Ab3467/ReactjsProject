5989fbb7d6b2966c979b5aaa5b5d42ab41d41169
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-26 11:40:08 +0500

    Changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -14,6 +14,7 @@ function App() {
       <div className="container">
         <Title subTitle= "Our Program" title="What We Offer"/>
         <Program />
+        <Title subTitle="Gallery" title="Campus Photos"/>
         <About/>
       </div>
     </div>
