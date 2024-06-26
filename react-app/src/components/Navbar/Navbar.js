2eacf5978303700aa696ca433f53708221eaf255
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-26 17:21:44 +0500

    Changes

--- react-app/src/components/Navbar/Navbar.js
+++ react-app/src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React,{useEffect, useState} from 'react'
 import "../Navbar/Navbar.css"
 import logo from "../../assets/logo.png"
+import Link from "react-scroll"
 
 export default function Navbar() {
   const [sticky,setsticky] =useState(false);
@@ -17,11 +18,11 @@ export default function Navbar() {
         <img src={logo} alt="" className='logo'/>
         <ul>
             <li><Link to="hero" smooth={true} offest={0} duration={500}>Home</Link></li>
-            <li><Link to="" smooth={true} offest={0} duration={500}>Program</Link></li>
-            <li><Link to="" smooth={true} offest={0} duration={500}>About us</Link></li>
-            <li><Link to="" smooth={true} offest={0} duration={500}>Campus</Link></li>
-            <li><Link to="" smooth={true} offest={0} duration={500}>Testimonials</Link></li>
-            <li><Link to="" smooth={true} offest={0} duration={500} className='btn'>Contact us</Link></li>
+            <li><Link to="programs" smooth={true} offest={0} duration={500}>Program</Link></li>
+            <li><Link to="about" smooth={true} offest={0} duration={500}>About us</Link></li>
+            <li><Link to="campus" smooth={true} offest={0} duration={500}>Campus</Link></li>
+            <li><Link to="testimonial" smooth={true} offest={0} duration={500}>Testimonials</Link></li>
+            <li><Link to="contact" smooth={true} offest={0} duration={500} className='btn'>Contact us</Link></li>
         </ul>
     </nav>
   )
