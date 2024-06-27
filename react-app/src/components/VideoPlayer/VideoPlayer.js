db349083e916ee5cf4e5a69553a4c9f5905068ed
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-27 11:12:51 +0500

    Changes

--- react-app/src/components/VideoPlayer/VideoPlayer.js
+++ react-app/src/components/VideoPlayer/VideoPlayer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./VideoPlayer.css"
 export default function VideoPlayer() {
   return (
-    <div className='video-player'>
+    <div className='video-player hide'>
       <video src={}></video>
     </div>
   )
