61c8f0a16b4806e3169b192a8a47492c2d8b8a47
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-27 11:33:48 +0500

    Changes

--- react-app/src/components/VideoPlayer/VideoPlayer.js
+++ react-app/src/components/VideoPlayer/VideoPlayer.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import "./VideoPlayer.css"
 import video from "../../assets/video.mp4"
 
 
 export default function VideoPlayer({playstate,setplaystate}) {
+
+    const player = useRef(null)
+    const closePlayer=(e)=>{
+        if(e.target === player.current){
+           setplaystate(false)
+        }
+    }
+
   return (
-    <div className={`video-player ${playstate ? '' : "hide"} `}>
+    <div className={`video-player ${playstate ? '' : "hide"} `} ref={player} onClick={closePlayer}>
       <video src={video} autoPlay muted controls></video>
     </div>
   )
